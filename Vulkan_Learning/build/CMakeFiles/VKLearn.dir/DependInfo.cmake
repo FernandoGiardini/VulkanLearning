8e3ce41eb0c99f9c2815af6c3f846329dd8b892f
Fernando Giardini <92555354+FernandoGiardini@users.noreply.github.com>
2024-04-03 17:25:55 -0300

    Add files via upload

---
+++ Vulkan_Learning/build/CMakeFiles/VKLearn.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/giardinif/C++/Vulkan_Learning/first_app.cpp" "CMakeFiles/VKLearn.dir/first_app.cpp.o" "gcc" "CMakeFiles/VKLearn.dir/first_app.cpp.o.d"
+  "/home/giardinif/C++/Vulkan_Learning/main.cpp" "CMakeFiles/VKLearn.dir/main.cpp.o" "gcc" "CMakeFiles/VKLearn.dir/main.cpp.o.d"
+  "/home/giardinif/C++/Vulkan_Learning/vkl_window.cpp" "CMakeFiles/VKLearn.dir/vkl_window.cpp.o" "gcc" "CMakeFiles/VKLearn.dir/vkl_window.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
