1c2e4987187192802366af0445d3361f4bfbe272
Fernando <fernandogiardining@outlook.com>
2024-04-05 19:46:50 -0300

    renaming file commit

--- vkl_window.cpp
+++ vkl_window.cpp
@@ -1,4 +1,5 @@
 #include "vkl_window.hpp"
+#include <stdexcept>
 
 namespace vkl {
 
@@ -21,4 +22,12 @@ namespace vkl {
 
         window = glfwCreateWindow(width,height,windowName.c_str(),nullptr,nullptr);
     }
+
+     void VKL_Window::createWindowSurface(VkInstance instance,VkSurfaceKHR* surface){
+        if (glfwCreateWindowSurface(instance,window,nullptr,surface) != VK_SUCCESS)
+        {
+            throw std::runtime_error("failed to create window surface");
+        }
+        
+     }
 }
--- vkl_window.hpp
+++ vkl_window.hpp
@@ -20,6 +20,8 @@ namespace vkl
 
             bool shouldClose() { return glfwWindowShouldClose(window);}
 
+            
+            void createWindowSurface(VkInstance instance,VkSurfaceKHR* surface);
         private:
 
             void initWindow();
